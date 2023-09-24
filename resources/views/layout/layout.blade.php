698c1cc573de60503b94372fc5748386957df650
ARVIN CEJALBO <arvincejalbo18@gmail.com>
2023-09-24 14:34:11 +0800

    temporary push

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -11,7 +11,7 @@
   <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.css" />
   <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.js"></script>
-
+  
 </head>
 <body>
     
