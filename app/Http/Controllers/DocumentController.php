1b555b9d00a1042e4fd08e2a18bb55a2d3b253fb
VIN <115197877+Binn12345@users.noreply.github.com>
2023-10-23 13:14:45 +0800

    'temporary'

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -3,12 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use App\Helpers\HelperClass;
 
 class DocumentController extends Controller
 {
     //
-    public function indexAction()
+    public function indexAction(User $user)
     {
-        return view('index');
+        $adminEmail = HelperClass::sessionSuperadmin($user);
+        $adminEmail = HelperClass::sessionjoin($adminEmail);
+
+        if($adminEmail instanceof RedirectResponse){
+            return $adminEmail; 
+        } 
+
+        $response = response()
+            ->view('admin.useraccount', $adminEmail)
+            ->header('Cache-Control', 'no-cache, no-store, max-age=0, must-revalidate')
+            ->header('Pragma', 'no-cache')
+            ->header('Expires', 'Fri, 01 Jan 1990 00:00:00 GMT');
+            
+        return $response;
     }
+
+
 }
